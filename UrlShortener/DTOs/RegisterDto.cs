0ca7aaf0789273633ed398c42f2a082bb4e516f1
Phạm Hoàng Anh <anhpham1723@gmail.com>
2025-12-30 21:42:25 +0700

    Update RegisterDto.cs

--- UrlShortener/DTOs/RegisterDto.cs
+++ UrlShortener/DTOs/RegisterDto.cs
@@ -4,6 +4,6 @@
     {
         public string Username { get; set; } = null!;
         public string Password { get; set; } = null!;
-        public string? Email { get; set; }
+        public string Email { get; set; } = null!;
     }
 }
